3c7066f58a7b89ae2feb68472b2f9d5db675f12c
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2024-01-19 16:30:17 +0530

    files and child nodes added

--- index.js
+++ index.js
@@ -2,4 +2,10 @@
 // heading.innerHTML="Siddharth"
 // console.log(heading)
 
-
+const parent=document.querySelector('.parent');
+// console.log(parent.children);
+console.log(parent.firstElementChild);
+const dayone=document.querySelector('.day');
+console.log(dayone);
+console.log(dayone.parentElement)
+console.log("nodes:",parent.childNodes);
